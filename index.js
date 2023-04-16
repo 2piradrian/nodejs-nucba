ca2a88f8ece31e0e8189a54fb46d21ff8d803bb3
Adrian Rodriguez <rodriguezcadr@gmail.com>
2023-04-16 10:33:42 -0300

    answer with date time

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 import fs from "fs";
 import inquirer from "inquirer";
+import DatePrompt from "inquirer-date-prompt";
+
+inquirer.registerPrompt("date", DatePrompt);
 
 const main = async () => {
 	console.log("Hello World");
+	const answers = await inquirer.prompt([
+		{
+			type: "input",
+			name: "name",
+			message: "Cual es tu nombre?",
+		},
+		{
+			type: "input",
+			name: "category",
+			message: "Que tipo de gasto es?",
+		},
+		{
+			type: "date",
+			name: "time",
+			message: "Cual es la fecha del gasto?",
+		},
+	]);
 };
 
 main();
