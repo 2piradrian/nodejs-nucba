d58aeb30eb0536cf824469a106ec49218e435671
Adrian Rodriguez <rodriguezcadr@gmail.com>
2023-07-09 23:26:38 -0300

    Unauthorized

--- src/controllers/expense.ts
+++ src/controllers/expense.ts
@@ -1,27 +1,57 @@
+import { RequestToken } from "./../data/types";
 import { Request, Response } from "express";
 import { ExpenseService } from "../services/expense";
 
 export const ExpenseController = {
 	async create(req: Request, res: Response) {
 		const { amount, name, userId } = req.body;
+
 		const newExpense = await ExpenseService.createExpense(amount, name, userId);
+
 		res.json(newExpense);
 	},
-	async getByExpenseId(req: Request, res: Response) {
+	async getByExpenseId(req: RequestToken, res: Response) {
 		const expense = await ExpenseService.getByExpenseId(Number(req.params.id));
+
+		if (req.userIdFromToken !== expense?.userId) {
+			return res.status(401).json({ message: "Unauthorized" });
+		}
+
 		res.json(expense);
 	},
-	async getByUserId(req: Request, res: Response) {
+	async getByUserId(req: RequestToken, res: Response) {
+		const userId = req.params.userId;
+
 		const expenses = await ExpenseService.getByUserId(Number(req.params.userId));
+
+		if (req!.userIdFromToken?.toString() !== userId) {
+			return res.status(401).json({ message: "Unauthorized" });
+		}
+
 		res.json(expenses);
 	},
-	async update(req: Request, res: Response) {
+	async update(req: RequestToken, res: Response) {
 		const { amount, name } = req.body;
+
+		const expense = await ExpenseService.getByExpenseId(Number(req.params.id));
+
+		if (req.userIdFromToken !== expense?.userId) {
+			return res.status(401).json({ message: "Unauthorized" });
+		}
+
 		const updatedExpense = await ExpenseService.update(Number(req.params.id), amount, name);
+
 		res.json(updatedExpense);
 	},
-	async delete(req: Request, res: Response) {
+	async delete(req: RequestToken, res: Response) {
+		const expense = await ExpenseService.getByExpenseId(Number(req.params.id));
+
+		if (req.userIdFromToken !== expense?.userId) {
+			return res.status(401).json({ message: "Unauthorized" });
+		}
+
 		const deletedExpense = await ExpenseService.delete(Number(req.params.id));
+
 		res.json(deletedExpense);
 	},
 };
--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,19 +1,28 @@
 import { Request, Response } from "express";
 import { UserService } from "../services/user";
+import { RequestToken } from "../data/types";
 
 export const UserController = {
-	async getUserById(req: Request, res: Response) {
+	async getUserById(req: RequestToken, res: Response) {
 		const user = await UserService.getUserById(Number(req.params.id));
+
+		if (req.userIdFromToken !== user?.id) {
+			return res.status(401).json({ message: "Unauthorized" });
+		}
+
 		if (!user) {
 			return res.status(404).json({ error: "User not found" });
 		}
+
 		res.json(user);
 	},
 
 	async login(req: Request, res: Response) {
 		try {
 			const { email, password } = req.body;
+
 			const token = await UserService.login(email, password);
+
 			res.json({ token });
 		} catch (error: any) {
 			res.status(500).json({ error: error.message });
@@ -23,7 +32,9 @@ export const UserController = {
 	async register(req: Request, res: Response) {
 		try {
 			const { email, password, name } = req.body;
+
 			const user = await UserService.register(name, email, password);
+
 			res.json(user);
 		} catch (error: any) {
 			res.status(500).json({ error: error.message });
